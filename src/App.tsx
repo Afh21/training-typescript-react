a67c8ee6ec46b35d9bed1b4e8fee55aa64bfc71e
Andres Felipe H <andres_felipe0929@hotmail.com>
2021-06-04 20:47:31 -0500

    Video 23 typescript-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+import "./App.css";
+
+const Heading = ({ title }: { title: string }) => <h2>{title}...</h2>;
+
+const Box: React.FunctionComponent = ({ children }) => <div>{children}</div>;
+
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item: string) => void;
+}> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
+interface Payload {
+  text: string;
+}
+interface Todo {
+  id: number;
+  done: boolean;
+  text: string;
+}
+
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
+
+const Incrementer: React.FunctionComponent<{
+  value: number;
+  setValue: React.Dispatch<React.SetStateAction<number>>;
+}> = ({ value, setValue }) => (
+  <Button onClick={() => setValue(value + 1)} title="Add" />
+);
+
+const Button: React.FunctionComponent<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & { title?: string }
+> = ({ title, children, ...rest }) => <button style={{ fontSize: "xx-large" }} {...rest}>{title ?? children}</button>;
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch("/db.json")
+      .then((resp) => resp.json())
+      .then((data) => setPayload(data));
+  }, []);
+
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+            done: false,
+          },
+        ];
+
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+
+      default:
+        throw new Error();
+    }
+  }, []);
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({ type: "ADD", text: newTodoRef.current.value });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
+  const [value, setValue] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Intro" />
+      <Box>
+        <h5>Hello again!</h5>
+      </Box>
+      <List items={["one", "two", "three"]} onClick={onListClick} />
+      <Box>{JSON.stringify(payload)}</Box>
+
+      <Incrementer value={value} setValue={setValue} />
+
+      <Heading title="Todos" />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+            Remove
+          </button>
+        </div>
+      ))}
+
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <Button onClick={onAddTodo}>Add Todo</Button>
+      </div>
     </div>
   );
 }
